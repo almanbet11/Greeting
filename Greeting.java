9090db5c8d5a1549fe4ab6af95fed1f91ae8d6f3
almanbet11 <44470593+almanbet11@users.noreply.github.com>
2019-04-19 11:29:31 +0600

    Create Greeting.java

---
+++ Greeting.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Greeting{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your name, please.")
+        String name = input.nextLine;
+        System.out.printf("%nHello, dear %s", name);
+    }
+}
